2e11fadcc581e579c23a2f211bc560c861bf6703
vinay polineni <vinaypolineni98@gmail.com>
2024-02-10 13:04:34 +0530

    updated

--- lambdafunction.py
+++ lambdafunction.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print ("event note")
+    print ("hello world")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
