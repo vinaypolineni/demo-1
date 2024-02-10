b1f9790dde61b5751f362c2a307ad45eb9b08545
vinay polineni <vinaypolineni98@gmail.com>
2024-02-10 13:51:14 +0530

    adding cli

--- lambdafunction.py
+++ lambdafunction.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-   
+    print ("hello world i'm from vs code")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
